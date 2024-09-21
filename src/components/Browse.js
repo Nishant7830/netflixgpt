836c9b2e54b82f2fd5dfbd7fa101f00ee82bea4d
Nishant7830 <nishant244241@gmail.com>
2024-09-21 17:42:19 +0530

    commit

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -3,12 +3,11 @@ import Header from "./Header";
 import useNowPlayingMovies from '../hooks/useNowPlayingMovies';
 import MainContainer from './MainContainer';
 import SeconadaryContainer from './SeconadaryContainer';
-
-
-
+import usePopularMovies from '../hooks/usePopularMovies';
 
 const Browse = () => {
   useNowPlayingMovies();
+  usePopularMovies();
 
   return (
     <div>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,3 +8,5 @@ export const API_OPTIONS =  {
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3Y2Q5YmQyMmY1ZWM2ZDBlN2ZlZjVkMmM4NzQ3MWFmNyIsIm5iZiI6MTcyNjczNTE0MS4yOTE0MzUsInN1YiI6IjY2ZTliOTEyODJmZjg3M2Y3ZDFlZGYyNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7OXecYZkvyR0WVOfiFtaAkUECv9tXHMqsNHWo6NtbBQ'
     }
   };
+
+  export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
